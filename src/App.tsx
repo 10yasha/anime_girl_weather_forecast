e8155de7cfef07568de8eb9116b3e269f4e13a14
Justin Huang <justin.huang@fotechsolutions.com>
2024-04-27 14:47:23 -0600

    testing api endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,31 @@
+import { cities } from "country-cities";
+
+interface city {
+  cityId: string;
+  name: string;
+  altName: string;
+  country: string;
+  featureCode: string;
+  adminCode: string;
+  population: number;
+  loc: {
+    type: string;
+    coordinates: number[];
+  };
+}
+
 function App() {
+  function getCities() {
+    return cities.all().filter((city) => city.name.match("Calgary"));
+  }
   return (
     <>
       <div>
-        <h1>Hello</h1>
+        <h1>{getCities()[0].name}</h1>
+        <h1>{getCities()[0].latitude}</h1>
+        <h1>{getCities()[0].longitude}</h1>
+
+        <h1>HELLO</h1>
       </div>
     </>
   );
