be231cb95101e57bd611df7376dbd7b914c138c2
Justin Huang <justin.huang@fotechsolutions.com>
2024-04-27 19:50:22 -0600

    searchbar work in progress, looks better

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { city } from "../interfaces/Interfaces";
+import "./SearchBar.css";
+
+import SearchIcon from "@mui/icons-material/Search";
 
 export default function SearchBar({
   placeholder,
@@ -8,12 +11,26 @@ export default function SearchBar({
   placeholder: string;
   data: city[];
 }) {
+  const [filteredData, setFilteredData] = useState([]);
   return (
     <div className="search">
-      <div className="searchInputs"></div>
-      <input type="text" placeholder={placeholder} />
-      <div className="searchIcon"> </div>
-      <div className="searchResult"></div>
+      <div className="searchInputs">
+        <input type="text" placeholder={placeholder} />
+        <div className="searchIcon">
+          <SearchIcon />
+        </div>
+      </div>
+      {filteredData.length != 0 && (
+        <div className="searchResult">
+          {data.map((value) => {
+            return (
+              <a className="searchItem" href="#">
+                <p>{value.name}</p>
+              </a>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
